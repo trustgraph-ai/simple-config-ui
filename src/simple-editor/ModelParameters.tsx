7cd460278a9b606fc2e6fc724f0da58ce58635ec
Jack Colquitt <126733989+JackColquitt@users.noreply.github.com>
2024-10-11 15:42:31 -0700

    Added Model names (#11)
    * Added Anthropic and Bedrock models
    
    * Added Cohere models
    
    * Added OpenAI models
    
    * Added Google AI Studio models
    
    * Added popular Ollama models
    
    * Added VertexAI models
    
    * Plug model list into model select menu
    
    ---------
    
    Co-authored-by: Cyber MacGeddon <cybermaggedon@gmail.com>

--- src/simple-editor/ModelParameters.tsx
+++ src/simple-editor/ModelParameters.tsx
@@ -1,52 +1,120 @@
 import React from 'react';
-import { TextField, Slider } from '@mui/material';
+import {
+    FormControl, InputLabel, Select, MenuItem, TextField, Slider
+} from '@mui/material';
+
+import modelsRaw from './models.json';
+const models = modelsRaw as { [ix : string ] : string[] };
 
 interface ModelParametersProps {
-  modelName: string;
-  temperature: number;
-  maxOutputTokens: number;
-  onModelNameChange: (value: string) => void;
-  onTemperatureChange: (value: number) => void;
-  onMaxOutputTokensChange: (value: number) => void;
+    modelName: string;
+    temperature: number;
+    maxOutputTokens: number;
+    onModelNameChange: (value: string) => void;
+    onTemperatureChange: (value: number) => void;
+    onMaxOutputTokensChange: (value: number) => void;
+    modelDeployment : string;
 }
 
 const ModelParameters: React.FC<ModelParametersProps> = ({
-  modelName,
-  temperature,
-  maxOutputTokens,
-  onModelNameChange,
-  onTemperatureChange,
-  onMaxOutputTokensChange,
+    modelName,
+    temperature,
+    modelDeployment,
+    maxOutputTokens,
+    onModelNameChange,
+    onTemperatureChange,
+    onMaxOutputTokensChange,
 }) => {
-  return (
-    <div>
-      <TextField
-        fullWidth
-        label="Model Name"
-        value={modelName}
-        onChange={(e) => onModelNameChange(e.target.value)}
-        margin="normal"
-      />
-      <div>
-        <p>Temperature: {temperature}</p>
-        <Slider
-          value={temperature}
-          onChange={(_, value) => onTemperatureChange(value as number)}
-          min={0}
-          max={1}
-          step={0.1}
-        />
-      </div>
-      <TextField
-        fullWidth
-        label="Max Output Tokens"
-        type="number"
-        value={maxOutputTokens}
-        onChange={(e) => onMaxOutputTokensChange(parseInt(e.target.value))}
-        margin="normal"
-      />
-    </div>
-  );
+
+    const availModels = models[modelDeployment];
+
+    const ModelList : React.FC<{
+        modelName : string;
+        availModels : string[];
+        onModelNameChange : (value: string) => void;
+    }> = ({ modelName, availModels, onModelNameChange}) => {
+
+        const readOnly = (availModels.length == 0);
+
+        if (availModels.length == 0) {
+            return (
+                <FormControl fullWidth disabled>
+
+                    <InputLabel id="model-name-label">Model</InputLabel>
+
+                    <Select
+                        labelId="model-name-label"
+                        id="model-name-select"
+                        value="n/a"
+                        label="Model deployment"
+                    >
+                        <MenuItem key="n/a" value="n/a">n/a</MenuItem>
+                    </Select>
+                </FormControl>
+            );
+
+        }
+
+        return (
+            <FormControl fullWidth>
+
+                <InputLabel id="model-name-label">Model</InputLabel>
+
+                <Select
+                    labelId="model-name-label"
+                    id="model-name-select"
+                    value={modelName}
+                    label="Model deployment"
+                    onChange={(e) => onModelNameChange(e.target.value)}
+                    inputProps={{ readOnly: readOnly }}
+                >
+                    {
+                        availModels.map(
+                            (v) => (
+                                <MenuItem key={v}
+                                    value={v}>
+                                    {v}
+                                </MenuItem>
+                            ))
+                    }
+                </Select>
+
+            </FormControl>
+
+        );
+    }
+
+    return (
+        <div>
+
+            <ModelList modelName={modelName} availModels={availModels}
+                onModelNameChange={onModelNameChange}
+            />
+
+            <div>
+                <p>Temperature: {temperature}</p>
+                <Slider
+                    value={temperature}
+                    onChange={
+                        (_, value) => onTemperatureChange(value as number)
+                    }
+                    min={0}
+                    max={1}
+                    step={0.1}
+                />
+            </div>
+            <TextField
+                fullWidth
+                label="Max Output Tokens"
+                type="number"
+                value={maxOutputTokens}
+                onChange={
+                    (e) => onMaxOutputTokensChange(parseInt(e.target.value))
+                }
+                margin="normal"
+            />
+        </div>
+    );
 };
 
 export default ModelParameters;
