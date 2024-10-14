b64910c78da1227689061990c679d0507f9b8913
Cyber MacGeddon <cybermaggedon@gmail.com>
2024-10-14 22:50:26 +0100

    Update K8s launch instructions

--- src/simple-editor/deployment/DeploymentInstructions.tsx
+++ src/simple-editor/deployment/DeploymentInstructions.tsx
@@ -4,22 +4,17 @@ import React from 'react';
 import {
     Typography, Box, Paper, Stack,
 } from '@mui/material';
-
 import { RocketLaunch } from '@mui/icons-material';
 
+import { useModelParamsStore } from '../state/ModelParams';
 import DeploymentCode from './DeploymentCode';
 import DeploymentStep from './DeploymentStep';
 
-interface DeploymentInstructionsProps {
-}
-
-const DeploymentInstructions: React.FC<DeploymentInstructionsProps> = ({
-}) => {
+const DeploymentInstructionsCompose = () => {
 
     return (
 
         <>
-
 
             <Box>
                 <Paper sx={{ minWidth: 375, mt: 2, p: 2 }} elevation={3}>
@@ -79,6 +74,69 @@ const DeploymentInstructions: React.FC<DeploymentInstructionsProps> = ({
         </>
 
     );
+
+}
+
+const DeploymentInstructionsKube = () => {
+
+    return (
+
+        <>
+
+            <Box>
+                <Paper sx={{ minWidth: 375, mt: 2, p: 2 }} elevation={3}>
+                    <Stack
+                        direction="row" spacing={2}
+                        alignItems="center"
+                        sx={{mb: 2}}
+                    >
+                        <RocketLaunch color="primary" fontSize="large"/>
+                        <Typography variant="h6" component="h3">
+                            Launch
+                        </Typography>
+                    </Stack>
+
+                    <DeploymentStep>
+
+                        When you download the deploy configuration, you will
+                        have a ZIP file containing all the configuration
+                        needed to launch TrustGraph on Kubernetes.
+                        Unzip the ZIP file...
+
+                    </DeploymentStep>
+
+                    <DeploymentCode>
+                        unzip deploy.zip
+                    </DeploymentCode>
+
+                    <DeploymentStep>
+
+                        and launch...
+
+                    </DeploymentStep>
+
+                    <DeploymentCode>
+                        kubectl apply -f resources.yaml
+                    </DeploymentCode>
+
+                </Paper>
+            </Box>
+
+        </>
+
+    );
+
+}
+
+const DeploymentInstructions: React.FC<{}> = ({
+}) => {
+
+    const platform = useModelParamsStore((state) => state.platform);
+
+    if (platform == "docker-compose" || platform == "podman-compose")
+        return <DeploymentInstructionsCompose/>;
+    else
+        return <DeploymentInstructionsKube/>;
 
 };
 
