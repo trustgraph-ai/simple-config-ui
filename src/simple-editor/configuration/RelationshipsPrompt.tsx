18a163521778513853fc12990e1636e6bbb70f84
JackColquitt <daniel@kalntera.ai>
2024-10-16 16:58:14 -0700

    Added instructions for prompt tailoring

--- src/simple-editor/configuration/RelationshipsPrompt.tsx
+++ src/simple-editor/configuration/RelationshipsPrompt.tsx
@@ -21,14 +21,22 @@ const RelationshipsPrompt = ({
 
         <Box sx={{ maxWidth: "45rem" }}>
 
-            <Typography variant="body1" component="p" gutterBottom>
-                The chunked text from the ingested documents is placed 
-                in the prompt as the variable <code>text</code>.
+        <Typography variant="body1" component="p" gutterBottom>
+                The purpose of the "Relationships" prompt is to find relationships
+                between entities, concepts, and values. These relationships are structured
+                in triples format as subject, predicate, and object. The boolean for
+                entity-object is true when the object of a triple is also an entity.
+                Below are tailoring instructions:
             </Typography>
 
             <Typography variant="body1" component="p" gutterBottom>
-                Note: The LLM response from this prompt must have the
-                following JSON structure to form correct RDF triples:
+                1. The chunked text from the ingested documents can be placed 
+                anywhere in the prompt using the variable <code>{'{text}'}</code>.
+            </Typography>
+
+            <Typography variant="body1" component="p" gutterBottom>
+                2. The LLM response from this prompt must have the
+                following JSON object structure to form correct RDF triples:
             </Typography>
 
             <pre>
@@ -39,6 +47,27 @@ const RelationshipsPrompt = ({
                 { '  "entity-object": boolean\n' }
                 { '}' }
             </pre>
+
+            <Typography variant="body1" component="p" gutterBottom>
+                3. Most LLMs will understand the instruction to return
+                an array of the above JSON objects without explicitly
+                using array notation in the prompt.
+            </Typography>
+
+            <Typography variant="body1" component="p" gutterBottom>
+                4. Be precise. Be succinct. Be consistent. In general, 
+                less is more with LLM prompts. 
+            </Typography>
+
+            <Typography variant="body1" component="p" gutterBottom>
+                5. The TrustGraph prompts services can parse code
+                delimiters such as ```json``` or simply ``````.
+            </Typography>
+
+            <Typography variant="body1" component="p" gutterBottom>
+                6. Modify the prompt text below. The modified prompt
+                will be packaged in the TrustGraph deployment automatically.
+            </Typography>
 
         </Box>
 
