2f14b67ac48c95d10a4496fb84dd0bb219c16e6d
cybermaggedon <cybermaggedon@gmail.com>
2024-12-28 12:17:56 +0000

    Add workbench instructions (#78)

--- src/simple-editor/deployment/Deployment.tsx
+++ src/simple-editor/deployment/Deployment.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { Box } from '@mui/material';
 
 //import { useModelParamsStore } from '../state/ModelParams';
+import { useOptionsStore, CONFIGURE_WORKBENCH } from '../state/Options';
 
 import DeploymentPlatform from './DeploymentPlatform';
 import DeploymentModel from './DeploymentModel';
@@ -11,6 +12,7 @@ import DeploymentConfig from './DeploymentConfig';
 import DeploymentInstructions from './DeploymentInstructions';
 import DeploymentVectorStore from './DeploymentVectorStore';
 import DeploymentGraphStore from './DeploymentGraphStore';
+import DeploymentWorkbench from './DeploymentWorkbench';
 
 interface DeploymentProps {
 }
@@ -18,41 +20,51 @@ interface DeploymentProps {
 const Deployment: React.FC<DeploymentProps> = ({
 }) => {
 
-  return (
+    const options = useOptionsStore((state) => state.options);
 
-      <>
+    return (
 
-          <Box className="deployment">
+        <>
 
-              <Box>
-                  <DeploymentPlatform/>
-              </Box>
+            <Box className="deployment">
 
-              <Box>
-                  <DeploymentModel/>
-              </Box>
+                <Box>
+                    <DeploymentPlatform/>
+                </Box>
 
-              <Box>
-                  <DeploymentVectorStore/>
-              </Box>
+                <Box>
+                    <DeploymentModel/>
+                </Box>
 
-              <Box>
-                  <DeploymentGraphStore/>
-              </Box>
+                <Box>
+                    <DeploymentVectorStore/>
+                </Box>
 
-              <Box>
-                  <DeploymentConfig/>
-              </Box>
+                <Box>
+                    <DeploymentGraphStore/>
+                </Box>
 
-              <Box>
-                  <DeploymentInstructions/>
-              </Box>
+                <Box>
+                    <DeploymentConfig/>
+                </Box>
 
-          </Box>
+                <Box>
+                    <DeploymentInstructions/>
+                </Box>
 
-      </>
+                {
+                    options.has(CONFIGURE_WORKBENCH) && (
+                        <Box>
+                            <DeploymentWorkbench/>
+                        </Box>
+                    )
+                }
 
-  );
+            </Box>
+
+        </>
+
+    );
 };
 
 export default Deployment;
