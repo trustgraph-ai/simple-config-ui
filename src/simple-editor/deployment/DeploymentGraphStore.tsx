d3becf402a4457267f9caccdb24021732511c291
JackColquitt <daniel@kalntera.ai>
2025-01-10 12:15:11 -0800

    Fixed FalkorDB license quote

--- src/simple-editor/deployment/DeploymentGraphStore.tsx
+++ src/simple-editor/deployment/DeploymentGraphStore.tsx
@@ -20,14 +20,12 @@ const GraphStoreConfiguration = () => {
         </Typography>
         <Typography variant="body2" sx={{ mt: 2 }}>
         <Paper elevation={8}>
-            <DeploymentQuote>
             "The Server Side Public License (SSPLv1) is designed to
             ensure that if you use FalkorDB as part of a service you
             make available to others (e.g., in the cloud or as an
             API), you are required to make the source code of your
             complete service available under the SSPLv1 license. This
             is similar to GPL but extends to server use."
-            </DeploymentQuote>
         </Paper>
         </Typography>
 
