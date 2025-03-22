f025a019d6203e55e4a09027dd974cf9052ed860
Cyber MacGeddon <cybermaggedon@gmail.com>
2025-03-22 20:28:02 +0000

    OCR layout looks about right

--- src/simple-editor/ocr/ConfigureOcr.tsx
+++ src/simple-editor/ocr/ConfigureOcr.tsx
@@ -6,6 +6,17 @@ import {
     RadioGroup, Radio,
 } from '@mui/material';
 
+import Grid from '@mui/material/Grid2';
+
+import FormatAlignLeftIcon from '@mui/icons-material/FormatAlignLeft';
+import FormatAlignCenterIcon from '@mui/icons-material/FormatAlignCenter';
+import FormatAlignRightIcon from '@mui/icons-material/FormatAlignRight';
+import LaptopIcon from '@mui/icons-material/Laptop';
+import TvIcon from '@mui/icons-material/Tv';
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+
 import PromptControls from './PromptControls';
 import PromptEditor from './PromptEditor';
 import { usePromptsStore } from '../state/Prompts';
@@ -13,46 +24,85 @@ import { usePromptsStore } from '../state/Prompts';
 const ConfigureOcr = ({
 }) => {
 
+  const [selectedValue, setSelectedValue] = React.useState('pdf-decode');
+  const [devices, setDevices] = React.useState(() => ['phone']);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedValue(event.target.value);
+  };
+
+
     return (<>
 
-<Box>
-        <Typography variant="h5" component="h2" gutterBottom>
-            Configure OCR
-        </Typography>
+        <Box sx={{width: "45rem"}}>
+            <Typography variant="h5" component="h2" gutterBottom>
+                Configure OCR
+            </Typography>
 
-        <FormControl>
+            <Grid container spacing={2}>
 
-          <FormLabel id="pdf-processing-mode-label">
-              PDF processing mode
-          </FormLabel>
+                <Grid
+                    size={2} display="flex"
+                    justifyContent="flex-end"
+                    alignItems="flex-start"
+                >
+                    <Radio
+                      checked={selectedValue === 'pdf-decode'}
+                      onChange={handleChange}
+                      value="pdf-decode"
+                      name="radio-buttons"
+                      inputProps={{ 'aria-label': 'A' }}
+                    />
+                </Grid>
+                <Grid item size={10}>
+                    PDF decode mode: PDF documents are decoded, and text is
+                    extracted.  No OCR is performed
+                </Grid>
 
-          <RadioGroup
-            row
-            aria-labelledby="pdf-processing-mode-label"
-            defaultValue="pdf-decode"
-            name="pdf-processing-mode-group"
-          >
+                <Grid
+                    size={2} display="flex"
+                    justifyContent="flex-end"
+                    alignItems="flex-start"
+                >
+                    <Radio
+                      checked={selectedValue === 'pdf-ocr'}
+                      onChange={handleChange}
+                      value="pdf-ocr"
+                      name="radio-buttons"
+                      inputProps={{ 'aria-label': 'A' }}
+                    />
+                </Grid>
+                <Grid size={10}>
+                    OCR processing with Tesseract.  Tesseract is a free,
+                    embedded OCR engine.  It is best-in-class free /
+                    open-source.
+                </Grid>
 
-            <FormControlLabel
-                value="pdf-decode" control={<Radio />}
-                label="PDF decode mode: PDF documents are decoded, and text is extracted.  No OCR is performed."
-            />
+                <Grid
+                    size={2} display="flex"
+                    justifyContent="flex-end"
+                    alignItems="flex-start"
+                >
+                    <Radio
+                      checked={selectedValue === 'pdf-ocr-mistral'}
+                      onChange={handleChange}
+                      value="pdf-ocr-mistral"
+                      name="radio-buttons"
+                      inputProps={{ 'aria-label': 'A' }}
+                    />
+                </Grid>
+                <Grid size={10}>
+                    OCR processing with the Mistral service.  Mistral is the
+                    best-in-class commercial OCR service.  You need a
+                    Mistral subscription.
+                </Grid>
 
-            <FormControlLabel
-                value="pdf-ocr-tesseract" control={<Radio />}
-                label="OCR processing with Tesseract.  Tesseract is a free, embedded OCR engine.  It is best-in-class free / open-source."
-            />
+            </Grid>
 
-            <FormControlLabel
-                value="pdf-ocr-mistral" control={<Radio />}
-                label="OCR processing with the Mistral service.  Mistral is the best-in-class commercial OCR service."
-            />
+        </Box>
 
-          </RadioGroup>
-
-        </FormControl>
-</Box>
     </>);
+
 };
 
 export default ConfigureOcr;
