6fa44088756778ec28fdcd60ed52f8260765daea
cybermaggedon <cybermaggedon@gmail.com>
2025-09-08 10:10:03 +0100

    Remove version menu unnecessary "reload" (#134)

--- src/simple-editor/state/Version.ts
+++ src/simple-editor/state/Version.ts
@@ -10,7 +10,10 @@ export interface Version {
 
 export interface VersionState {
     version: Version;
+    versions: Version[];
+    versionsLoaded: boolean;
     setVersion: (v: any) => void;
+    setVersions: (v: Version[]) => void;
 }
 
 export const useVersionStateStore = create<VersionState>()(
@@ -21,7 +24,10 @@ export const useVersionStateStore = create<VersionState>()(
             template: "",
             version: "",
         },
+        versions: [],
+        versionsLoaded: false,
         setVersion: (v) => set({ version: v }),
+        setVersions: (v) => set({ versions: v, versionsLoaded: true }),
     })
 );
 
