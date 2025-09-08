33028fe30bd857431a4272a0035908e7f8bb0e6d
cybermaggedon <cybermaggedon@gmail.com>
2025-09-08 11:42:27 +0100

    Remove unused options (#136)

--- src/simple-editor/deployment/Deployment.tsx
+++ src/simple-editor/deployment/Deployment.tsx
@@ -3,7 +3,7 @@ import { Box } from '@mui/material';
 
 import { useConfigurationStateStore } from '../state/Configuration';
 import {
-    useOptionsStore, CONFIGURE_WORKBENCH, CONFIGURE_OCR,
+    useOptionsStore, CONFIGURE_OCR,
 } from '../state/Options';
 
 import DockerCompose from './DockerCompose';
@@ -47,7 +47,6 @@ import FalkorDb from './FalkorDb';
 import GatewayKube from './GatewayKube';
 
 import Workbench from './Workbench';
-import NoWorkbench from './NoWorkbench';
 
 import DocumentRag from './DocumentRag';
 
@@ -179,10 +178,7 @@ const Deployment: React.FC<DeploymentProps> = ({
     if (config.graphStore == "falkordb")
         deploymentProcedures.push(<FalkorDb/>);
 
-    if (options.has(CONFIGURE_WORKBENCH))
-        deploymentProcedures.push(<Workbench/>);
-    else
-        deploymentProcedures.push(<NoWorkbench/>);
+    deploymentProcedures.push(<Workbench/>);
 
     if (config.platform == "docker-compose" ||
         config.platform == "podman-compose")
